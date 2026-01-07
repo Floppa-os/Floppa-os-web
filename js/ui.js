cce4aa7b7847bc699515e2df6f08353f15751cae
Mickredset <aivliev782@gmail.com>
2026-01-07 10:34:18 +0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -24,3 +24,34 @@ function createWindow(appId, title, content) {
 // Закрытие окна
 function closeWindow(appId) {
     const
+// Перетаскивание окон
+function dragElement(el) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    el.querySelector('.app-header').onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        el.style.top = (el.offsetTop - pos2) + "px";
+        el.style.left = (el.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
